5b42751d18b8c95fb0d9f6f997d774af3783d736
Christopher Francis White <whitecf@bcs.org.uk>
2018-02-17 20:10:38 +0000

    Updated to latest generic blcksqnc code.

--- mas4sqnc.asm
+++ mas4sqnc.asm
@@ -1,28 +1,10 @@
 ;**********************************************************************
 ;                                                                     *
-;    Description:   Controller for occupation block with positional   *
-;                   train detector at exit.                           *
+;    Description:   Controller for multiple aspect colour light       *
+;                   signal and occupation block with positional train *
+;                   detector at block exit.                           *
 ;                                                                     *
-;                   Receives train detection state from previous (in  *
-;                   rear) controller which it uses as entry detector  *
-;                   for occupation block.                             *
-;                   Sends value of signal aspect (increment of local  *
-;                   value of signal aspect) along with special speed  *
-;                   indication and block reversed to previous         *
-;                   controller.                                       *
-;                                                                     *
-;                   Receives value of signal aspect to be displayed   *
-;                   along with special speed indication and block     *
-;                   reversed from next (in advance) controller.       *
-;                   Sends train detection state to next controller.   *
-;                                                                     *
-;                   If no data is received from next controller link  *
-;                   input is treated as a level input indicating      *
-;                   to display a stop aspect or to cycle aspect from  *
-;                   stop to clear at fixed intervals after the        *
-;                   passing of a train.                               *
-;                                                                     *
-;                   Outputs aspect display for United Kingdom         *
+;                   This is a specialisation for United Kingdom       *
 ;                   Railways 4 aspect MAS signals.                    *
 ;                                                                     *
 ;    Author:        Chris White                                       *
@@ -31,7 +13,7 @@
 ;                                                                     *
 ;**********************************************************************
 ;                                                                     *
-;    Copyright (C) 2017  Monitor Computing Services Ltd.              *
+;    Copyright (C) 2018  Monitor Computing Services Ltd.              *
 ;                                                                     *
 ;    This program is free software; you can redistribute it and/or    *
 ;    modify it under the terms of the GNU General Public License      *
@@ -54,15 +36,15 @@
 ;**********************************************************************
 ;                                                                     *
 ;                            +---+ +---+                              *
-;             Emitter  <- RA2|1  |_| 18|RA1                           *
-;              Sensor  -> RA3|2      17|RA0                           *
-;          !Detecting  <- RA4|3      16|                              *
+;                 Red  <- RA2|1  |_| 18|RA1 -> Green                  *
+;              Yellow  <- RA3|2      17|RA0 -> Second Yellow          *
+;          !Detecting <-> RA4|3      16|                              *
 ;                            |4      15|                              *
-;                            |5      14|      Aspects:                *
-;                         RB0|6      13|RB7 -> Red                    *
-; Next <-> / !Inhibit  -> RB1|7      12|RB6 -> Yellow                 *
-;            Previous <-> RB2|8      11|RB5 -> Double yellow          *
-;       Special speed  -> RB3|9      10|RB4 -> Green                  *
+;                            |5      14|                              *
+;     !Latch Signal On -> RB0|6      13|RB7 <-> Next / <- !Inhibit    *
+;       !Line reversed -> RB1|7      12|RB6 <-> Previous              *
+;   Line bidirectional -> RB2|8      11|RB5 ->  !Emitter              *
+;         Normal speed -> RB3|9      10|RB4 <-  Sensor                *
 ;                            +---------+                              *
 ;                                                                     *
 ;**********************************************************************
@@ -72,20 +54,19 @@
 ; Configuration directives and constant definitions
 ;**********************************************************************
 #include "blcksqnc/blcksqnc_def.inc"
+
 ; Aspect values
 ASPDYFLG    EQU     ASPCLFLG
 ASPGREEN    EQU     ASPCLR
 
 ; Aspect output constants
-REDOUT      EQU     7           ; Red aspect output bit
-REDMSK      EQU     B'10000000' ; Mask for red aspect output bit
-YELLOWOUT   EQU     6           ; Yellow aspect output bit
-YELLOWMSK   EQU     B'01000000' ; Mask for yellow aspect output bit
-DOUBLEOUT   EQU     4           ; Double yellow aspect output bit
-DBLYLWMSK   EQU     B'00010000' ; Mask for double yellow aspect output bit
-GREENOUT    EQU     5           ; Green aspect output bit
-GREENMSK    EQU     B'00100000' ; Mask for green aspect output bit
+REDMSK      EQU     B'00000100' ; Mask for red aspect output
+YELLOWMSK   EQU     B'00001000' ; Mask for yellow aspect output
+DBLYLWMSK   EQU     B'00001001' ; Mask for double yellow aspect output
+GREENMSK    EQU     B'00000010' ; Mask for green aspect output
+BLANKMSK    EQU     B'00000000' ; Mask for blank aspect output
   
+
 ;**********************************************************************
 ; Variable registers
 ;**********************************************************************
@@ -97,21 +78,33 @@ endRAM      EQU afterRAM - 1
     error "This program ran out of RAM!"
 #endif
 
+
 ;**********************************************************************
 ; EEPROM initialisation
 ;**********************************************************************
 #include "blcksqnc/blcksqnc_rom.inc"
 
+
 ;**********************************************************************
 ; Code
 ;**********************************************************************
+
+; Include serial link interface macros
+;  - Serial link bit timing is performed by link service routines
+#define CLKD_SERIAL
+#include "blcksqnc/utility/asyn_srl.inc"
+#include "blcksqnc/utility/link_hd.inc"
 #include "blcksqnc/blcksqnc_cod.inc"
+
 
 ;**********************************************************************
 ; Subroutine to return aspect output mask in accumulator
 ;**********************************************************************
-GetAspectMask
-    movf    aspOut,W        ; Get aspect display value
+GetAspectOutput
+    btfss   inputs,SPDBIT   ; Skip if normal speed input set ...
+    bsf     nxtCntlr,SPDFLG ; ... else display flashing warning aspects
+
+    movf    aspVal,W        ; Get aspect display value
     btfsc   STATUS,Z        ; Skip if not zero (not red) ...
     retlw   REDMSK          ; ... else display red aspect
 
@@ -120,17 +113,17 @@ GetAspectMask
     retlw   GREENMSK        ; ... else display green aspect
 
     movlw   HALFSEC
-    andwf   secCount,w      ; Test for flashing aspect blanking period
+    andwf   secCount,W      ; Test for flashing aspect blanking period
 
     movlw   YELLOWMSK       ; Display yellow aspect
-    btfsc   aspOut,ASPDYFLG ; Skip if double yellow not required ...
+    btfsc   aspVal,ASPDYFLG ; Skip if double yellow not required ...
     movlw   DBLYLWMSK       ; ... else display double yellow aspect
 
-    btfss   nxtCntlr,SPDFLG ; Skip if next signal at special speed ...
+    btfss   nxtCntlr,SPDFLG ; Skip if next signal not at normal speed ...
     return                  ; ... else display (double) yellow aspect
     btfss   STATUS,Z        ; Skip if aspect blanking period ...
     return                  ; ... else display (double) yellow aspect
-    retlw   0               ; Blank aspect display
+    retlw   BLANKMSK        ; Blank aspect display
 
 ;**********************************************************************
 ; End of source code
